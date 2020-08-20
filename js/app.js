a2f0e95363e280dcd801f9bca7191ab14730e293
Edson Cabina <edson.cabina@jupiter.co.ao>
2020-08-20 15:33:57 +0100

    M2A8

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
-var myApp = angular.module('myApp',['ngMessages','ngResource']);
+var myApp = angular.module('myApp',[]);
 
-myApp.controller("mainController",function($scope,$log,$filter,$resource){
-    console.log($resource);
+/**
+ myApp.controller("mainController",function($scope,$log){
+
+    $log.log($scope);    
+    
+});
+ */
+/**
+ * Minificar
+myApp.controller("mainController",['$scope','$log',function($scope,$log){
+
+    $log.log($scope);    
+    
+}]);
+myApp.controller("mainController",["$scope","$log",function(o,l){l.log(o)}]);
+ */
+
+myApp.controller("mainController",function($scope,$log){
+
+    $log.log($scope);    
     
 });
