8e9364f1fa2a32cbea598d1d5150141ce7612aa2
Edson Cabina <edson.cabina@jupiter.co.ao>
2020-08-20 15:01:46 +0100

    M4A6

--- js/app.js
+++ js/app.js
@@ -1,23 +1,6 @@
-var myApp = angular.module('myApp',['ngMessages']);
+var myApp = angular.module('myApp',['ngMessages','ngResource']);
 
-myApp.controller("mainController",function($scope,$log,$filter){
+myApp.controller("mainController",function($scope,$log,$filter,$resource){
+    console.log($resource);
     
-    /**
-     //$log= metodo semelhante ao console do js
-     $log.log("Olá");
-    $log.info("Informação");
-    $log.warn("Aviso");
-    $log.debug("Debug");
-    $log.error("Erro");;
-     */
-    $scope.nome = "Edson";
-    $scope.nomeFormatado = $filter('uppercase')($scope.nome);
-
-    $log.info($scope.nome);
-    $log.info($scope.nomeFormatado);
-
-
-
-    
-    //console.log($log);
 });
